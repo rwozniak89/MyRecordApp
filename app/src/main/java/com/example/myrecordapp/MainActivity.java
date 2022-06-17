36dbe3819c967dcf3b341103b1e022654ac9c110
Radosław Woźniak <rwozniak89@gmail.com>
2022-06-17 23:00:27 +0200

    aaaa

---
+++ app/src/main/java/com/example/myrecordapp/MainActivity.java
@@ -0,0 +1,385 @@
+package com.example.myrecordapp;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener
+{
+
+    // Initializing all variables..
+    private TextView startTV, stopTV, playTV, stopplayTV, statusTV, statusTV2, statusTV3;
+
+    Spinner spinner;
+
+
+
+
+    // creating a variable for medi recorder object class.
+    private MediaRecorder mRecorder;
+
+    // creating a variable for mediaplayer class
+    private MediaPlayer mPlayer;
+
+    // string variable is created for storing a file name
+    private static String mFileName = null;
+
+    private static LinkedList<String> ListFileNames = new LinkedList<String>();
+
+    // constant for storing audio permission
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListFileNames.clear();
+        ListFileNames.add("<<wybierz>>");
+
+        //String place = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+
+        // initialize all variables with their layout items.
+        statusTV = findViewById(R.id.idTVstatus);
+        statusTV2 = findViewById(R.id.idTVstatus2);
+        statusTV3 = findViewById(R.id.idTVstatus3);
+        startTV = findViewById(R.id.btnRecord);
+        stopTV = findViewById(R.id.btnStop);
+        playTV = findViewById(R.id.btnPlay);
+        stopplayTV = findViewById(R.id.btnStopPlay);
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+
+        spinner = (Spinner) findViewById(R.id.planets_spinner);
+
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+        ArrayAdapter ad = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                ListFileNames);
+
+        // set simple layout resource file
+        // for each item of spinner
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+
+        // Set the ArrayAdapter (ad) data on the
+        // Spinner which binds data to spinner
+        spinner.setAdapter(ad);
+// Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        Log.d("Files", "Path: " + path);
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        Log.d("Files", "Size: "+ files.length);
+        for (int i = 0; i < files.length; i++)
+        {
+            if(files[i].isFile())
+            {
+                //ListFileNames.add(path + '/' + files[i].getName());
+                ListFileNames.add(files[i].getAbsolutePath());
+            }
+
+            Log.d("Files", "FileName:" + files[i].getName());
+        }
+
+
+        startTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start recording method will
+                // start the recording of audio.
+                startRecording();
+            }
+        });
+        stopTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pause Recording method will
+                // pause the recording of audio.
+                pauseRecording();
+
+            }
+        });
+        playTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // play audio method will play
+                // the audio which we have recorded
+                playAudio();
+            }
+        });
+        stopplayTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // pause play method will
+                // pause the play of audio
+                pausePlaying();
+            }
+        });
+
+    }
+
+    private String getNameByDateTime(){
+
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yymmdd_hhmmss");
+        String strDate = "A_" + dateFormat.format(date);
+        //String strDate = "Audio_"; // + dateFormat.format(date);
+        return strDate;
+    }
+
+
+    class PrimeRun implements Runnable {
+        long minPrime;
+        PrimeRun(long minPrime) {
+            this.minPrime = minPrime;
+        }
+
+        public void run() {
+            long startTime = System.currentTimeMillis();
+            long elapsedTime = 0L;
+
+            while (elapsedTime < 5*1000) {
+                //perform db poll/check
+                elapsedTime = (new Date()).getTime() - startTime;
+
+                statusTV3.setText(Long.toString(elapsedTime));
+            }
+
+            pauseRecording();
+        }
+    }
+
+
+    private void startRecording() {
+        // check permission method is used to check
+        // that the user has granted permission
+        // to record nd store the audio.
+        if (CheckPermissions()) {
+
+            // setbackgroundcolor method will change
+            // the background color of text view.
+            stopTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+            startTV.setBackgroundColor(getResources().getColor(R.color.gray));
+            playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+            stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+
+            // we are here initializing our filename variable
+            // with the path of the recorded audio file.
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/" + getNameByDateTime() +  ".3gp";
+            //mFileName += "/audio.3gp";
+            ListFileNames.add(mFileName);
+
+            // below method is used to initialize
+            // the media recorder class
+            mRecorder = new MediaRecorder();
+
+            // below method is used to set the audio
+            // source which we are using a mic.
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+
+            // below method is used to set
+            // the output format of the audio.
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            // below method is used to set the
+            // audio encoder for our recorded audio.
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            // below method is used to set the
+            // output file location for our recorded audio
+            mRecorder.setOutputFile(mFileName);
+            try {
+                // below method will prepare
+                // our audio recorder class
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e("TAG", "prepare() failed");
+            }
+            // start method will start
+            // the audio recording.
+            mRecorder.start();
+            statusTV.setText("Recording Started");
+            PrimeRun p = new PrimeRun(143);
+            new Thread(p).start();
+
+
+
+
+
+
+        } else {
+            // if audio recording permissions are
+            // not granted by user below method will
+            // ask for runtime permission for mic and storage.
+            RequestPermissions();
+        }
+    }
+
+    // Performing action when ItemSelected
+    // from spinner, Overriding onItemSelected method
+    @Override
+    public void onItemSelected(AdapterView<?> arg0,
+                               View arg1,
+                               int position,
+                               long id)
+    {
+        String name = ListFileNames.get(position);
+        if(position != 0) {
+            statusTV2.setText(name);
+            mFileName = name;
+        }
+        // make toastof name of course
+        // which is selected in spinner
+        Toast.makeText(getApplicationContext(),
+                        name,
+                        Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // this method is called when user will
+        // grant the permission for audio recording.
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length > 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean CheckPermissions() {
+        // this method is used to check permission
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        // this method is used to request the
+        // permission for audio recording and storage.
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+
+    public void playAudio() {
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
+        // for playing our recorded audio
+        // we are using media player class.
+        mPlayer = new MediaPlayer();
+        try {
+            // below method is used to set the
+            // data source which will be our file name
+            mPlayer.setDataSource(mFileName);
+
+            // below method will prepare our media player
+            mPlayer.prepare();
+
+            // below method will start our media player.
+            mPlayer.start();
+            statusTV.setText("Recording Started Playing");
+        } catch (IOException e) {
+            Log.e("TAG", "prepare() failed");
+        }
+    }
+
+    public void pauseRecording() {
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
+        // below method will stop
+        // the audio recording.
+        mRecorder.stop();
+
+        // below method will release
+        // the media recorder class.
+        mRecorder.release();
+        mRecorder = null;
+        statusTV.setText("Recording Stopped");
+    }
+
+    public void pausePlaying() {
+        // this method will release the media player
+        // class and pause the playing of our recorded audio.
+        mPlayer.release();
+        mPlayer = null;
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        statusTV.setText("Recording Play Stopped");
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+        statusTV2.setText("xx");
+    }
+
+    //@Override
+    //public void onNothingSelected(AdapterView<?> adapterView) {
+    //    // Auto-generated method stub
+    //}
+}
